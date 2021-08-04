2439abad29c025352891215c735f3da82d08bd45
Shijen.nelliulathil <shijen.nelliulathil@happiestminds.com>
2021-08-04 11:56:19 +0530

    4th commit

--- app/src/main/java/com/cjn/extlib/MainActivity.kt
+++ app/src/main/java/com/cjn/extlib/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity{
     companion object   {
         fun showToast(context: Context, msg:String) {
             Toast.makeText(context,msg, Toast.LENGTH_SHORT).show()
